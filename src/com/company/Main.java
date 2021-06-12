0bffd16e205fde692815de70676a72a3b58eeb28
bel-mojo <belmojo.j93@gmail.com>
2021-06-12 14:20:34 -0400

    the final commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,11 @@ package com.company;
 public class Main {
     public static void main(String[] args){
         Car audi=new Car();
-        audi.color="black";
-        audi.model="r8";
-        audi.door="4";
-        audi.number="00789";
 
-        test cases for the color of the cha
-                test case for immediate requirement
-                is written down
+        audi.color="red";
+        audi.number="2568";
+        audi.model="a5";
+        audi.door="2";
+
     }
 }
