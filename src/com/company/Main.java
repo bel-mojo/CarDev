8d670384c8f36c0f228d4013b88417573afaf4bf
bel-mojo <belmojo.j93@gmail.com>
2021-06-12 14:06:39 -0400

    New updated test cases

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,9 @@ public class Main {
         audi.model="r8";
         audi.door="4";
         audi.number="00789";
+
+        test cases for the color of the cha
+                test case for immediate requirement
+                is written down
     }
 }
