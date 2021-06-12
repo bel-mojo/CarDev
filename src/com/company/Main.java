ce2dd9bf91169f020468b82c026402f49e1a1b3b
bel-mojo <belmojo.j93@gmail.com>
2021-06-12 13:11:49 -0400

    final audi r8 car

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,5 +3,9 @@ package com.company;
 public class Main {
     public static void main(String[] args){
         Car audi=new Car();
+        audi.color="black";
+        audi.model="r8";
+        audi.door="4";
+        audi.number="00789";
     }
 }
